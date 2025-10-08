2c51c156a7fd47f3196a2f960ede2c0347da899e
Kostubh Khandelwal <khandelwalkostubh@gmail.com>
2025-10-08 15:04:48 +0530

    WIP:  Add hand_drawing_node in perception pipeline.

--- follow_traj_moveit/launch/camera.launch.py
+++ follow_traj_moveit/launch/camera.launch.py
@@ -5,8 +5,10 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import ComposableNodeContainer, LoadComposableNodes
+from launch_ros.actions import ComposableNodeContainer, LoadComposableNodes, Node
 from launch_ros.descriptions import ComposableNode
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 
 def launch_setup(context, *args, **kwargs):
@@ -14,80 +16,105 @@ def launch_setup(context, *args, **kwargs):
     if context.environment.get("DEPTHAI_DEBUG") == "1":
         log_level = "debug"
 
+    mr_manip_share = FindPackageShare('mr_manipulator')
+
+    # Launch configuration variables
     params_file = LaunchConfiguration("params_file")
     namespace = LaunchConfiguration("namespace").perform(context)
     name = LaunchConfiguration("name").perform(context)
     rectify_rgb = LaunchConfiguration("rectify_rgb")
     use_apriltag = LaunchConfiguration("use_apriltag")
+    use_sim_time = LaunchConfiguration('use_sim_time', default='False')
 
-    return [
-        ComposableNodeContainer(
-            name=f"camera_container",
-            namespace=namespace,
-            package="rclcpp_components",
-            executable="component_container",
-            composable_node_descriptions=[
-                ComposableNode(
-                    package="depthai_ros_driver",
-                    plugin="depthai_ros_driver::Camera",
-                    name=name,
-                    namespace=namespace,
-                    parameters=[
-                        params_file,
-                    ],
-                )
-            ],
-            arguments=["--ros-args", "--log-level", log_level],
-            output="screen",
-        ),
-        LoadComposableNodes(
-            condition=IfCondition(rectify_rgb),
-            target_container=f"{namespace}/camera_container",
-            composable_node_descriptions=[
-                ComposableNode(
-                    package="image_proc",
-                    plugin="image_proc::RectifyNode",
-                    name="rectify_color_node",
-                    namespace=namespace,
-                    parameters=[params_file],
-                    remappings=[
-                        ("image", f"{name}/rgb/image_raw"),
-                        ("camera_info", f"{name}/rgb/camera_info"),
-                        ("image_rect", f"{name}/rgb/image_rect"),
-                        (
-                            "image_rect/compressed",
-                            f"{name}/rgb/image_rect/compressed",
-                        ),
-                        (
-                            "image_rect/compressedDepth",
-                            f"{name}/rgb/image_rect/compressedDepth",
-                        ),
-                        (
-                            "image_rect/theora",
-                            f"{name}/rgb/image_rect/theora",
-                        ),
-                    ],
-                )
-            ],
-        ),
-        LoadComposableNodes(
-            condition=IfCondition(use_apriltag),
-            target_container=f"{namespace}/camera_container",
-            composable_node_descriptions=[
-                ComposableNode(
-                    package="apriltag_ros",
-                    plugin="AprilTagNode",
-                    name="apriltag",
-                    namespace=namespace,
-                    parameters=[params_file],
-                    remappings=[
-                        ("image_rect", f"{name}/rgb/image_rect"),
-                        ("camera_info", f"{name}/rgb/camera_info"),
-                    ],
-                )
-            ],
-        ),
-    ]
+    model_path = PathJoinSubstitution([mr_manip_share, 'models', 'xgboost_model.p'])
+
+    camera_node_container = ComposableNodeContainer(
+        name=f"camera_container",
+        namespace=namespace,
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="depthai_ros_driver",
+                plugin="depthai_ros_driver::Camera",
+                name=name,
+                namespace=namespace,
+                parameters=[
+                    params_file,
+                ],
+            )
+        ],
+        arguments=["--ros-args", "--log-level", log_level],
+        output="screen",
+    )
+
+    load_rectify_node = LoadComposableNodes(
+        condition=IfCondition(rectify_rgb),
+        target_container=f"{namespace}/camera_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="image_proc",
+                plugin="image_proc::RectifyNode",
+                name="rectify_color_node",
+                namespace=namespace,
+                parameters=[params_file],
+                remappings=[
+                    ("image", f"{name}/rgb/image_raw"),
+                    ("camera_info", f"{name}/rgb/camera_info"),
+                    ("image_rect", f"{name}/rgb/image_rect"),
+                    (
+                        "image_rect/compressed",
+                        f"{name}/rgb/image_rect/compressed",
+                    ),
+                    (
+                        "image_rect/compressedDepth",
+                        f"{name}/rgb/image_rect/compressedDepth",
+                    ),
+                    (
+                        "image_rect/theora",
+                        f"{name}/rgb/image_rect/theora",
+                    ),
+                ],
+            )
+        ],
+    )
+
+    load_apriltag_node = LoadComposableNodes(
+        condition=IfCondition(use_apriltag),
+        target_container=f"{namespace}/camera_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="apriltag_ros",
+                plugin="AprilTagNode",
+                name="apriltag",
+                namespace=namespace,
+                parameters=[params_file],
+                remappings=[
+                    ("image_rect", f"{name}/rgb/image_rect"),
+                    ("camera_info", f"{name}/rgb/camera_info"),
+                ],
+            )
+        ],
+    )
+
+    camera_layer = [camera_node_container, load_rectify_node, load_apriltag_node]
+
+    hand_drawing_node = Node(
+        package='mr_manipulator',
+        executable='hand_drawing',
+        name='hand_drawing',
+        namespace=namespace,
+        output='screen',
+        parameters=[{
+            'use_sim_time': use_sim_time,
+            'model_path': model_path,
+            'camera_topic': f"{name}/rgb/image_rect",
+            'camera_info_topic': f"{name}/rgb/camera_info",
+        }]
+    )
+
+    perception_layer = [hand_drawing_node]
+    return camera_layer + perception_layer
 
 
 def generate_launch_description():
@@ -102,6 +129,7 @@ def generate_launch_description():
         ),
         DeclareLaunchArgument("rectify_rgb", default_value="true"),
         DeclareLaunchArgument("use_apriltag", default_value="true"),
+        DeclareLaunchArgument('use_sim_time', default_value='False'),
     ]
 
     return LaunchDescription(
